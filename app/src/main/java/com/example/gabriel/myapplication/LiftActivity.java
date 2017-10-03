57561fb27e4f2f52b1b5926e5841a4b89ed5ed7c
Gabriel <Gabrielliland@outlook.com>
2017-10-03 21:16:33 +0200

    first try

---
+++ app/src/main/java/com/example/gabriel/myapplication/LiftActivity.java
@@ -0,0 +1,13 @@
+package com.example.gabriel.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LiftActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lift);
+    }
+}
